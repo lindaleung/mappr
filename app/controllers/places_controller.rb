37e042f9ce8adb2a51616c22442e7257e74779be
Linda Leung <lindaleung@Tonys-MacBook-Pro.local>
2013-11-17 15:56:28 -0500

    add form for place; linked pages together

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,4 +2,23 @@ class PlacesController < ApplicationController
 	def index
 		@place = Place.last
 	end
+
+	def new
+		@place = Place.new
+	end
+
+	def create
+		# does the equivalent of [Place.create(:title => 'omg', ...)] in the terminal
+		# create method needs to define place_params below
+		# create a new line in the Place db with the place_params, store it in @place_params
+		@place = Place.create( place_params )
+		redirect_to root_path
+	end
+
+	# anything under private is internal to site not accessible by public via url
+	private
+
+	def place_params
+		params.require(:place).permit(:title, :address)
+	end
 end
